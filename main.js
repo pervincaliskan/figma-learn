3aee6b147a54b6a7a403c72a997d484e64175444
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2023-08-22 22:37:08 +0300

    14.Gün slider

---
+++ main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  width: 284 * 4.3,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  }
+})
