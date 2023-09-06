a9f798f7b7d562c0d9ce04a882ba2940907698a2
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2023-09-06 10:14:13 +0300

    notes added

---
+++ $spacer = 1;.js
@@ -0,0 +1,22 @@
+ $spacer = 1;
+
+0 -  0
+1 - $spacer * .25 = 1 * .25     = .25rem 
+2 - $spacer * .5 = 1 * .5       = .5rem
+3 - $spacer                     = 1rem
+4 - $spacer * 1.5 = 1 * 1.5     = 1.5rem
+5 - $spacer * 3 = 1 * 3         = 3rem
+
+currentIndex = 0
+
+slides = [
+  slid1,
+  slid2,
+  slide3,
+  slid4
+]
+
+for (let index = 0; index < slides.length; index++) {
+  const slide = slides[index];
+  
+}
