502ff0a99b0454b3f6f413e4034f1f8292042786
Pervin Çalışkan <52761014+pervincaliskan@users.noreply.github.com>
2023-09-06 10:11:21 +0300

    main.js added

--- main.js
+++ main.js
@@ -27,3 +27,57 @@ function handleHamburgerButtonClick(event) {
 if (hamburgerButtonEl) {
   hamburgerButtonEl.addEventListener('click', handleHamburgerButtonClick)
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.getElementById("prev-btn");
+  const nextBtn = document.getElementById("next-btn");
+  let currentIndex = 0;
+
+  // Başlangıçta ilk slaytı göster
+  slides[currentIndex].style.display = "block";
+
+  prevBtn.addEventListener("click", function () {
+    // currentIndex = 1
+    if (currentIndex > 0) {
+      slides[currentIndex].style.display = "none";
+      currentIndex--;
+      // currentIndex = 0
+      slides[currentIndex].style.display = "block";
+    } else {
+      // currentIndex = 0
+      currentIndex = slides.length - 1;
+      // currentIndex = 3
+
+      for (let index = 0; index < slides.length; index++) {
+        const slide = slides[index];
+        if (index === currentIndex) {
+          slide.style.display = 'block';
+        } else {
+          slide.style.display = 'none';
+        }
+      }
+    }
+  });
+
+  nextBtn.addEventListener("click", function () {
+    // // currentIndex = 0
+    if (currentIndex < slides.length - 1) {
+      slides[currentIndex].style.display = "none";
+      currentIndex++;
+      slides[currentIndex].style.display = "block";
+    } else {
+      currentIndex = 0;
+
+      for (let index = 0; index < slides.length; index++) {
+        const slide = slides[index];
+        if (index === currentIndex) {
+          slide.style.display = 'block';
+        } else {
+          slide.style.display = 'none';
+        }
+      }
+
+    }
+  });
+});
